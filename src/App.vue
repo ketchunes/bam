99db952350bb973929b2c2a22f1ac088b974e61c
catGirl_seeker <danildenisov572@gmail.com>
2025-07-27 15:41:58 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <MyHello title="Кросовки Puma" :price="9500">
+    <button class="btn">lol</button>
+  </MyHello>
+  <MyHello title="Кросовки lol" :price="8400">
+    <div>lol</div>
+  </MyHello>
+  <MyHello title="Кросовки kek" :price="7300"></MyHello>
+  <MyHello title="Кросовки apchi" :price="6200"></MyHello>
+</template>
+
+<script setup>
+  import MyHello from "./components/MyHello.vue";
+</script>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-center items-center border-2 mt-5">
+    <div class="flex flex-col items-center">
+      <div class="text-2xl">Товар:</div>
+      <div class="text-2xl">{{ title }}</div>
+      <div class="text-2xl mt-5">Цена: {{ price }}</div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    title: String,
+    price: Number,
+  });
+</script>
