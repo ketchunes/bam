f05ef287d0f3dbe3083dba0e1f52f2c63bf76337
catGirl_seeker <danildenisov572@gmail.com>
2025-08-04 17:32:27 +0500

    button-animated

--- src/components/Goods.vue
+++ src/components/Goods.vue
@@ -2,10 +2,10 @@
   <div class="w-4/5 p-5 m-auto bg-white">
     <div class="text-3xl font-semibold">Все кроссовки</div>
     <div class="grid grid-cols-4">
-      <div class="w-fit flex justify-between" v-for="pop in pops">
+      <div class="w-fit flex justify-between" v-for="(pop, index) in pops">
         <div class="mt-5 rounded-xl p-3 hover:-translate-y-2 hover:shadow-xl transition">
           <div class="relative cursor-pointer">
-            <img class="absolute top-5 left-5 w-10" src="../public/img/like-1.png" alt="" />
+            <img class="absolute top-5 left-5" src="../public/img/like-1.png" alt="" />
             <img class="m-auto" :src="pop.img" alt="" />
             <div class="w-80">{{ pop.description }}</div>
             <div class="flex justify-between items-end">
@@ -13,7 +13,9 @@
                 <div class="text-slate-400">Цена:</div>
                 <div class="font-bold text-xl">{{ pop.price }}</div>
               </div>
-              <button class="btn bg-white rounded-xl border-2 border-gray-300 text-xl text-gray-500">+</button>
+              <button @click="itemsBasketBtnState[index] = !itemsBasketBtnState[index]" class="active:translate-1 active:shadow-xl rounded-xl transition cursor-pointer">
+                <img class="w-10" :src="itemsBasketBtnState[index] ? '../public/img/btn-2.png' : '../public/img/btn-1.png'" alt="" />
+              </button>
             </div>
           </div>
         </div>
@@ -23,6 +25,8 @@
 </template>
 
 <script setup>
+  import { ref, onBeforeMount } from "vue";
+
   const pops = [
     {
       img: "/img/boots-1.png",
@@ -85,4 +89,6 @@
       price: "11 299 руб.",
     },
   ];
+
+  const itemsBasketBtnState = ref([]);
 </script>
